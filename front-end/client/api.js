edf96a592b34d25f6f7d2a59d055e37b38663f9b
Prasurjya Sarma <82230151+Prasurjyasarma@users.noreply.github.com>
2025-04-08 01:01:24 +0530

    updated api

--- front-end/client/api.js
+++ front-end/client/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  //   baseURL: "http://localhost:8000/api",
-  baseURL: "http://192.168.0.100:8000/api",
+  baseURL: "http://localhost:8000/api",
 });
 
 export default api;
